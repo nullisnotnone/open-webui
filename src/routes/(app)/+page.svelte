b92b1179a7443d1e33da55f386d03858ad97bfe4
sunchuang <sunchuang@91160.com>
2025-03-19 22:48:16 +0800

    fix: UI

--- src/routes/(app)/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Chat from '$lib/components/chat/Chat.svelte';
-	import Help from '$lib/components/layout/Help.svelte';
-</script>
-{#if 0> 1}
-<Help />
-{/if}
-
-<Chat />
